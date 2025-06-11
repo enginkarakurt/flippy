732d2dc3b262e387eb7bf1bb61f937e13a1f0301
enginkarakurt <155079545+enginkarakurt@users.noreply.github.com>
2025-06-11 10:22:19 +0200

    Get version automatically for frontend

--- src/renderer/src/components/Header.tsx
+++ src/renderer/src/components/Header.tsx
@@ -1,11 +1,11 @@
 import ThemeToggle from './ThemeToggle'
-
 function Header(): React.JSX.Element {
+  const version = 'v' + import.meta.env.PACKAGE_VERSION
   return (
     <header className="p-4 flex flex-row justify-between items-center border-b-1">
       <div className="flex justify-center items-end">
         <h1 className="text-4xl font-bold text-blue-500">Flippy</h1>
-        <p>v1.0.0</p>
+        <p>{version}</p>
       </div>
       <ThemeToggle />
     </header>
